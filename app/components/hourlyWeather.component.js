f040a32ac593f8ee8fcac84c251c764d55cb6b19
Bryant <bryant.m.wang@gmail.com>
2017-06-17 19:36:05 -0700

    Add hourlyWeather display component and associated group component.

---
+++ app/components/hourlyWeather.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Moment from 'moment-timezone';
+
+class HourlyWeather extends React.Component {
+
+  return (
+    <div className="hourly-weather">
+    </div>
+  );
+
+}
+
+export default HourlyWeather
---
+++ app/components/hourlyWeatherGroup.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class HourlyWeatherGroup extends React.Component {
+
+  return (
+    <div className="hourly-weather-group">
+    </div>
+  );
+
+}
+
+export default HourlyWeatherGroup
