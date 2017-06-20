975e520096275aa13a8d3c1b138d9d2b0e8b8dab
Bryant <bryant.m.wang@gmail.com>
2017-06-19 20:27:52 -0700

    Make controls for bootstrap carousel work on detailedWeatherGroup.

--- app/components/detailedWeatherGroup.component.js
+++ app/components/detailedWeatherGroup.component.js
@@ -33,18 +33,18 @@ class DetailedWeatherGroup extends React.Component {
     }
     return (
       <div className="detailed-weather-group col-12">
-        <div className="carousel slide" data-ride="carousel">
+        <div id="weatherCarousel" className="carousel slide" data-ride="carousel">
           <div className="carousel-inner" role="listbox">
             <HourlyWeatherGroup
               hourly={this.props.hourly}
             />
             {detailedWeathers}
           </div>
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <a className="carousel-control-prev" href="#weatherCarousel" role="button" data-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
               <span className="sr-only">Previous</span>
           </a>
-          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <a className="carousel-control-next" href="#weatherCarousel" role="button" data-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="sr-only">Next</span>
           </a>
