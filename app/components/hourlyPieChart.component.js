9cf6f0993910fe3657871152e6c1d12660ed8b21
Bryant <bryant.m.wang@gmail.com>
2017-08-12 20:56:49 -0700

    Add additional pie charts to display temperature, layer original pie charts over new ones to provide a border effect, rotate charts so that the top half reflects most of the typical daylight hours.

--- app/components/hourlyPieChart.component.js
+++ app/components/hourlyPieChart.component.js
@@ -7,16 +7,17 @@ class HourlyPieChart extends React.Component {
       for(let idx = 0; idx < (25 - this.props.data.length); idx++) {
         let divStyle = { backgroundColor: 'Beige', transform: 'rotate(0deg)' };
 
-        let rotation = 0;
-        rotation = counter * 15;
+        let rotation = counter * 15;
         divStyle.transform = `rotate(${rotation}deg)`;
 
         let reactElement = <div className="pie-slice" key={idx} style={divStyle}></div>;
 
         if(counter <= 11) {
-          rightSlices.push(reactElement);
+          rightSlices[0].push(reactElement);
+          rightSlices[1].push(reactElement);
         } else {
-          leftSlices.push(reactElement);
+          leftSlices[0].push(reactElement);
+          leftSlices[1].push(reactElement);
         }
         counter++;
       }
@@ -25,8 +26,8 @@ class HourlyPieChart extends React.Component {
   }
 
   render() {
-    let rightSlices = [];
-    let leftSlices = [];
+    let rightSlices = [[], []];
+    let leftSlices = [[], []];
 
     if(typeof this.props.data !== 'undefined') {
       let counter = 0;
@@ -37,26 +38,47 @@ class HourlyPieChart extends React.Component {
 
       for(let hour of this.props.data) {
         if(typeof hour !== 'string') {
-          let divStyle = { backgroundColor: 'Beige', transform: 'rotate(0deg)' };
+          let divStyle1 = { backgroundColor: 'Beige', transform: 'rotate(0deg)' };
+          let divStyle2 = { backgroundColor: 'Beige', transform: 'rotate(0deg)' };
 
           let icons = ['clear-day', 'clear-night', 'partly-cloudy-day', 'partly-cloudy-night', 'cloudy', 'rain', 'sleet', 'snow', 'wind', 'fog'];
           let iconColors = ['Yellow', 'Yellow', 'LightSteelBlue', 'LightSteelBlue', 'Black', 'DodgerBlue', 'DodgerBlue', 'DodgerBlue', 'Plum', 'LightSlateGray'];
           let iconIndex = icons.indexOf(hour.icon);
           if(iconIndex >= 0) {
-            divStyle.backgroundColor = iconColors[iconIndex];
+            divStyle1.backgroundColor = iconColors[iconIndex];
           } else {
-            divStyle.backgroundColor = 'Beige';
+            divStyle1.backgroundColor = 'Beige';
           }
 
-          let rotation = 0;
-          rotation = counter * 15;
-          divStyle.transform = `rotate(${rotation}deg)`;
+          let rotation = counter * 15;
+          divStyle1.transform = `rotate(${rotation}deg)`;
+          divStyle2.transform = `rotate(${rotation}deg)`;
 
-          let reactElement = <div className="pie-slice" key={hour.time} style={divStyle}></div>;
-          if(counter <= 11) {
-            rightSlices.push(reactElement);
+          let borderTemp;
+          if(hour.temperature > 100) {
+            borderTemp = 1;
+          } else if (hour.temperature < 25) {
+            borderTemp = .01;
           } else {
-            leftSlices.push(reactElement);
+            borderTemp = (Math.floor(hour.temperature - 24) / 100) * 1.25;
+          }
+
+          let borderRed = Math.floor(255 * borderTemp);
+          let borderGreenInit = Math.floor(100 * (2 - 2 * borderTemp));
+          let borderGreen = borderGreenInit < 0 ? -borderGreenInit : borderGreenInit;
+          let borderBlue = Math.floor(255 * (1 - borderTemp));
+
+          // console.log(`temp: ${hour.temperature}, borderRed: ${borderRed}, borderGreen: ${borderGreen}, borderBlue: ${borderBlue}`);
+          divStyle2.backgroundColor = `rgb(${borderRed}, ${borderGreen}, ${borderBlue})`;
+
+          let reactElement1 = <div className="pie-slice" key={hour.time} style={divStyle1}></div>;
+          let reactElement2 = <div className="pie-slice" key={hour.time} style={divStyle2}></div>;
+          if(counter <= 11) {
+            rightSlices[0].push(reactElement1);
+            rightSlices[1].push(reactElement2);
+          } else {
+            leftSlices[0].push(reactElement1);
+            leftSlices[1].push(reactElement2);
           }
           counter++;
         }
@@ -73,17 +95,26 @@ class HourlyPieChart extends React.Component {
 
             <div className="pie-chart">
               <div className="pie-half">
-                {rightSlices}
+                {rightSlices[0]}
               </div>
               <div className="pie-half">
-                {leftSlices}
+                {leftSlices[0]}
               </div>
             </div>
 
-            <div className="pie-time">12 AM</div>
-            <div className="pie-time">6 AM</div>
+            <div className="pie-chart">
+              <div className="pie-half">
+                {rightSlices[1]}
+              </div>
+              <div className="pie-half">
+                {leftSlices[1]}
+              </div>
+            </div>
+
             <div className="pie-time">12 PM</div>
             <div className="pie-time">6 PM</div>
+            <div className="pie-time">12 AM</div>
+            <div className="pie-time">6 AM</div>
 
           </div>
         </div>
